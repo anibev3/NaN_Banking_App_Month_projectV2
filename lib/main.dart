1c0b86691b2ca0b78cc74e36268ccfcc5f456d2f
anibe201 <anibe201@gmail.com>
2023-05-06 11:42:54 +0000

    Base code

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("NaWary"),
+        ),
+        body: const Center(
+          child: Text("Bonjour / Hello world !"),
+        ),
+      ),
+    );
+  }
+}
